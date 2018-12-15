abcd6f19fc5f909496e5f05f729471294201b082
pcharazinski <pawelcharazinski@gmail.com>
2018-12-16 00:20:41 +0100

    Rozbudowa

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require ("express");
 const app = express ();
 app.set ("view engine", "pug");
+
+const indexRoutes = require ('./routes/index');
+app.use ('/', indexRoutes);
+
 const port = 3000;
-app.listen (port, ()=>console.log(`Jestem podlaczona do serwera ${port} bijacz`));
+app.listen (port, () =>console.log(`Jestem podlaczona do serwera ${port} bijacz`));
