4a5e22416180ad6e9bf6a4bee515238e1ffc2215
pcharazinski <pawelcharazinski@gmail.com>
2018-12-15 23:45:29 +0100

    bitch this a new me

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require ("express");
 const app = express ();
 app.set ("view engine", "pug");
 const port = 3000;
-app.listen (port, ()=>console.log(`Jestem podłaczony do serwera ${port} bijacz`));
+app.listen (port, ()=>console.log(`Jestem podlaczona do serwera ${port} bijacz`));
