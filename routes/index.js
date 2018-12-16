9b303f10a3a552d30f43eacc762f6fe2ca410e77
pcharazinski <pawelcharazinski@gmail.com>
2018-12-16 01:58:38 +0100

    Ładne to nawet

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router();
+
 router.get ('/', (req,res) => res.render ('index'));
+
+router.post ('/addme', (req, res) => console.log(req.body.title));
+
 module.exports = router;
