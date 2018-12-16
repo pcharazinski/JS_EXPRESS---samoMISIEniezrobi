a9447c0399f50dc98aa1d75bdc73f93e10dd91be
pcharazinski <pawelcharazinski@gmail.com>
2018-12-16 02:28:48 +0100

    Połaczenie z baza danych mongodb/mongoose/mlab

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require ("express");
+const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
+mongoose.connect ('mongodb://admin:admin62D.@ds121652.mlab.com:21652/samomisieniezrobi').then (() => {
+console.log('ejj... chyba polaczylam sie z baza danych, lol');
+
+});
 const app = express ();
+
 app.use (bodyParser.urlencoded({extended:false}));
 app.set ("view engine", "pug");
 const indexRoutes = require ('./routes/index');
